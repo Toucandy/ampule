1d81308579e0f6fced17f6b90d43c2c25e9a309d
pershin <pershin2010@gmail.com>
2021-01-05 00:40:15 +0300

    new setup.py and more

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup
 
 setup(
    name = 'ampule',
-   version = '0.5',
-   description='Automated MatPlotLib',
+   packages = ['ampule'],
+   version = '0.9',
+   description='Ampule (Automated MatPlotLib) -- serial non-interactive processing and plotting of tabular data',
+   #url = 'https://github.com/Toucandy/ampule',
+   #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+   license='MIT',
    author = 'Ilya Pershin',
    author_email='pershin2010@gmail.com',
-   packages = ['ampule'],
-   install_requires = ['pandas'],
-   data_files=[('build', ['build/ampule_plot.mk', 'build/ampule_config.mk'])]
+   install_requires = ['matplotlib', 'pandas', 'parse'],
+   data_files=[('ampule', ['ampule_plot.mk', 'ampule_config.mk'])],
+   keywords = ['matplotlib', 'plotting', 'pandas'],
+   classifiers=[
+     'Development Status :: 4 - Beta',
+     'Intended Audience :: Science/Research',
+     'Topic :: Scientific/Engineering :: Information Analysis',
+     'License :: OSI Approved :: MIT License',
+     'Programming Language :: Python :: 3',
+  ],
 )
