32d95d5f106d5720109b007a734d679054968e2a
pershin <pershin2010@gmail.com>
2021-01-05 19:28:35 +0300

    test script are commented

--- figs/py/hermite.py
+++ figs/py/hermite.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 from ampule import Ampule, search_mask
 
+#This command must be at the beginning of every Ampule script
 ampule = Ampule(*sys.argv)
 
 plt.ylim(-10, 10)
@@ -20,9 +21,12 @@ plt.grid()
 plt.legend()
 plt.title('Hermite Polynomials')
 
+#Simple wrapper for plt.savefig(), removing the embedding of the date in the output files
+#as well as creating some necessary paths
 ampule.savefig(plt, 'hermite')
 plt.clf()
 
 ###############################################################################
 
+#This command must be at the end of every Ampule script
 ampule.flush_deps()
--- figs/py/poly.py
+++ figs/py/poly.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 from ampule import Ampule, search_mask
 
+#This command must be at the beginning of every Ampule script
 ampule = Ampule(*sys.argv)
 
 data, _ = ampule.load(f'dat/poly.dat')
@@ -11,7 +12,11 @@ data, _ = ampule.load(f'dat/poly.dat')
 plt.plot(data.x, data.f, label = r'$f(x)$')
 plt.plot(data.x, data.g, label = r'$g(x)$')
 plt.plot(data.x, data.h, label = r'$h(x)$')
+
+#Also you can use arithmetic on columns
 plt.plot(data.x, data.f + data.g/2 + data.h/6, label = r'$(f + g/2 + h/6)(x)$')
+#For more complex cases, use functions like numpy.sin() or do the thing manually using
+#map(), zip() and list comprehensions
 
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
@@ -19,9 +24,12 @@ plt.grid()
 plt.legend()
 plt.title('Linear scale')
 
+#Simple wrapper for plt.savefig(), removing the embedding of the date in the output files
+#as well as creating some necessary paths
 ampule.savefig(plt, 'linear')
 plt.clf()
 
+#You are not limited to one picture per script. Let's create another
 ###############################################################################
 
 plt.plot(data.x, data.f, label = r'$f(x)$')
@@ -40,4 +48,5 @@ plt.clf()
 
 ###############################################################################
 
+#This command must be at the end of every Ampule script
 ampule.flush_deps()
