93a545b2f99357e971d21a44bcbfb46955a3ff9b
pershin <pershin2010@gmail.com>
2022-01-24 12:10:44 +0300

    version updated

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name = 'ampule',
     packages = ['ampule'],
-    version = '0.9',
+    version = '0.9.5',
     description='Ampule (Automated MatPlotLib) is a minimalistic tool designed for repeated\
 non-interactive processing and plotting of tabular data',
     url = 'https://github.com/Toucandy/ampule',
-    download_url = 'https://github.com/Toucandy/ampule/archive/v_09.tar.gz',
+    download_url = 'https://github.com/Toucandy/ampule/archive/v_095.tar.gz',
     license='MIT',
     author = 'Ilya Pershin',
     author_email='pershin2010@gmail.com',
